11166a6042a91f153b3bab3fe4f38e84a5a278a7
Stankaninets <STANV27@TDBFG.COM>
2017-06-30 17:35:28 -0400

    adding playground directory

---
+++ playground/json.js
@@ -0,0 +1,39 @@
+// var obj = {
+//   name : 'Vlad'
+// };
+//
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof obj);
+// console.log(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// var personString = '{"name": "Vlad", "age": 46}';
+// var personObject = JSON.parse(personString);
+// console.log(typeof personObject);
+// console.log(personObject);
+// console.log(personObject.name);
+// console.log(personObject.age);
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+};
+
+// creating a string from the Javascript object above
+var originalNoteString = JSON.stringify(originalNote);
+
+//write originalNoteString as the file notes.json to the FileSystem
+fs.writeFileSync('notes.json', originalNoteString);
+
+// now read from the file notes.json into the temp string var
+var noteString = fs.readFileSync('notes.json');
+
+// now convert noteString string into a Javascript object
+var noteObject = JSON.parse(noteString);
+console.log(typeof noteObject);
+console.log(noteObject);
+console.log(noteObject.title);
+console.log(noteObject.body);
