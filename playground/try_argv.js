4bf2f9dfed3c4651fa105a25a159481355b3603f
Vlad Stankaninets <vlad.stankaninets@gmail.com>
2017-10-30 02:52:05 -0400

    Adding details to try_yargs.js'

--- playground/try_argv.js
+++ playground/try_argv.js
@@ -1,15 +1,27 @@
-const yargs = require('yargs').usage('Usage: $0 --command [str]')
-    .example('node $0 --command=[list,add,delete] book records').demand(['command']).argv;
+const yargs = require('yargs')
+    .usage('Usage: node $0 --action[str]==[list,add,delete] --title[str]=book name')
+    .demand(['action', 'title']).argv;
 
-// console.log(yargs);
+console.log(yargs);
 // console.log(yargs._);
+// console.log(yargs._[0]);
+// const check = yargs.title;
+console.log('Title is', typeof yargs.title);
 
-if (yargs.command === 'add') {
+if (yargs.action === 'add' && !!yargs.title && yargs.title !== true) {
 
-    console.log('This is an ADD command');
+    console.log('This is an ADD action | the Book name: ', yargs.title);
+
+} else if (yargs.action === 'list' && !!yargs.title && yargs.title !== true) {
+
+    console.log('The is a LIST action | The Book name: ', yargs.title);
+
+} else if (yargs.action === 'delete' && !!yargs.title && yargs.title !== true) {
+
+    console.log('This is a DELETE action |The Book name: ', yargs.title);
 
 } else {
 
-    console.log('The has been an error');
+    console.log('There has been an error');
 
 }
