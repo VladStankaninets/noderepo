7f39a2381be9f86db6049c3699adb2458056c913
Vlad Stankaninets <vlad.stankaninets@gmail.com>
2017-12-06 22:48:09 -0500

    eslinted

--- playground/closures.js
+++ playground/closures.js
@@ -1,19 +1,18 @@
 var myFunc = function myFunc (nm, slr) {
-  
-  var name = nm;
-  var salary = slr;
-  console.log(`Hi ${name} Your salary ${salary}`);
-  
-    function salaryIncrease () {
-        salary += 10000 ;  
-        return(`Hi ${name} Your salary ${salary}`);
-    };
-  
-return salaryIncrease;
 
+    var name = nm;
+    var salary = slr;
+    console.log(`Hi ${name} Your salary ${salary}`);
+
+    var salaryIncrease = function salaryIncrease () {
+        salary += 10000;
+        return `Hi ${name} Your salary ${salary}`;
+    };
+
+    return salaryIncrease;
 };
 
-var SalInc = myFunc('Vlad', 50000);
-console.log(SalInc());
-console.log(SalInc());
-console.log(SalInc());
+var salInc = myFunc('Vlad', 50000);
+console.log(salInc());
+console.log(salInc());
+console.log(salInc());
